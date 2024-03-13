42e91c3b867fcbe752e7e625746aa80177d36385
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-13 11:08:45 +0300

    add some examples for testing

--- src/test/java/com/colak/springreactivewebtestclienttutorial/student/controller/StudentControllerTest.java
+++ src/test/java/com/colak/springreactivewebtestclienttutorial/student/controller/StudentControllerTest.java
@@ -1,6 +1,7 @@
 package com.colak.springreactivewebtestclienttutorial.student.controller;
 
 import com.colak.springreactivewebtestclienttutorial.student.dto.StudentDto;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -8,13 +9,14 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
 class StudentControllerTest {
     @LocalServerPort
     private int port;
@@ -25,35 +27,47 @@ class StudentControllerTest {
     @Test
     @Order(1)
     void testEnrollStudent() {
-        StudentDto StudentDto = new StudentDto(null, "John Doe");
+        StudentDto studentDto = new StudentDto(null, "John Doe");
 
         webTestClient.post()
                 .uri("/api/v1/student/enroll")
-                .body(Mono.just(StudentDto), StudentDto.class)
+                .contentType(MediaType.APPLICATION_JSON)
+                // We can use bodyValue() or body() to post an object
+                .bodyValue(studentDto)
+                // .body(Mono.just(StudentDto), StudentDto.class)
                 .exchange()
                 .expectStatus().isCreated()
-                .expectBody(String.class).isEqualTo("Student enrolled successfully. Student ID: 1");
+                .expectBody(String.class)
+                // We can use value() to access result body
+                .value(resultBody -> log.info("Result is : {}", resultBody))
+                .isEqualTo("Student enrolled successfully. Student ID: 1");
     }
 
     @Test
     @Order(2)
     void testUpdateStudent() {
-        StudentDto StudentDto = new StudentDto(null, "Jane Doe");
+        StudentDto studentDto = new StudentDto(null, "Jane Doe");
 
         webTestClient.post()
                 .uri("/api/v1/student/enroll")
-                .body(Mono.just(StudentDto), StudentDto.class)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(studentDto)
+                // .body(Mono.just(studentDto), StudentDto.class)
                 .exchange()
                 .expectStatus().isCreated()
-                .expectBody(String.class).isEqualTo("Student enrolled successfully. Student ID: 2");
+                .expectBody(String.class)
+                .isEqualTo("Student enrolled successfully. Student ID: 2");
 
         StudentDto updatedStudentDto = new StudentDto(2L, "Updated Name");
         webTestClient.put()
                 .uri("/api/v1/student/update/" + updatedStudentDto.getId())
-                .body(Mono.just(updatedStudentDto), StudentDto.class)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(studentDto)
+                // .body(Mono.just(updatedStudentDto), StudentDto.class)
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("Student with ID 2 updated successfully");
+                .expectBody(String.class)
+                .isEqualTo("Student with ID 2 updated successfully");
     }
 
     @Test
